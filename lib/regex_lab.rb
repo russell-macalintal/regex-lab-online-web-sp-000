041bc48e86f64928bc7ea18cdffac9d8eeda088b
Russell Macalintal <russell.macalintal@gmail.com>
2019-09-30 04:47:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,9 +23,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/\d/).length == 10
-    true
-  else
-    false
-  end
+  phone.scan(/\d/).length == 10
 end
