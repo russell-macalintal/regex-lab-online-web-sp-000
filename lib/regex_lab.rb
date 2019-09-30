752c83e3430b252cb6296690b0857817ee09ba22
Russell Macalintal <russell.macalintal@gmail.com>
2019-09-30 04:20:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiouAEIOU]/) == nil
+  if word.match(/^[aeiou]/i) == nil
     return false
   else
     return true
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un+\w*)
+  
 end
 
 def words_five_letters_long(text)
