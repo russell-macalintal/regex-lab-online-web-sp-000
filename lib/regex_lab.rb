0b2d2bb924ab89d82de672bbf71a1062487f24bc
Russell Macalintal <russell.macalintal@gmail.com>
2019-09-30 04:11:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  return true
+  if word.match(/^[aeiou]/) == nil
+    return nil
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
